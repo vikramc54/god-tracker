21bd2afc705cee762217c4745a240216dbaa033e
Vikram C <vikrambnglr@gmail.com>
2025-06-30 19:54:14 +0530

    chore: add a filter for categories in non-debug mode

--- src/app/api/expense/categories/route.ts
+++ src/app/api/expense/categories/route.ts
@@ -3,12 +3,14 @@ import db from "@/lib/mongodb";
 import { Expense } from "../route";
 import { NextResponse } from "next/server";
 
+const isDebug = process.env.NEXT_IS_DEBUG === "true";
+
 export async function GET() {
     try {
         const user = await requireSession();
         if (!user) return new Response("Unauthorized", { status: 401 });
 
-        const categories = await db.collection<Expense>("expenses").distinct("categories");
+        const categories = await db.collection<Expense>("expenses").distinct("categories", isDebug ? {} : { isDebug: false });
 
         return NextResponse.json(categories);
     } catch (err) {
